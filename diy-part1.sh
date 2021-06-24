5cb7e674f279d77cbe89eaec9b6b8657c37522c4
derye <164745936@qq.com>
2021-06-24 21:18:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-cd package/lean/  
-git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/luci-app-vssr.git  
+cd package/lean/
+
+https://github.com/xiaorouji/openwrt-passwall.git
